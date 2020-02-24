eac4325d73102478ca9643b792014a07528e3574
Tim 'mithro' Ansell <me@mith.ro>
2020-02-23 17:01:42 -0800

    Updating data to v0.0-6140-g81fb4f00c
    Updated to 0.0.post6140 based on 81fb4f00c2cfe13814765968e09931ffa93b5138 from https://git.llvm.org/git/compiler-rt.git/.
    > commit 81fb4f00c2cfe13814765968e09931ffa93b5138
    > Author: Chris Bieneman <beanz@apple.com>
    > Date:   Fri Nov 6 23:19:29 2015 +0000
    >
    >     [CMake] Need to filter ${arch}/*.c builtins as well as ${arch}/*.S builtins.
    >
    >     This was broken in r248542 when I refactored this to support builtins where ${arch} didn't match the directory prefix (i.e. armv7s).
    >
    >     git-svn-id: https://llvm.org/svn/llvm-project/compiler-rt/trunk@252365 91177308-0d34-0410-b5e6-96231b3b80d8
    >
    
    Updated using v0.0-14-g06e06e3 from https://github.com/litex-hub/litex-data-auto

--- litex/data/software/compiler_rt/__init__.py
+++ litex/data/software/compiler_rt/__init__.py
@@ -2,12 +2,12 @@ import os.path
 __dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
 data_location = os.path.join(__dir__, "data")
 src = "https://git.llvm.org/git/compiler-rt.git/"
-git_hash = "1dd2eb5ffc3a86c9d38ec6beaef179f00460be70"
-git_describe = "v0.0-11908-g1dd2eb5ff"
-version_str = "0.0.post11908"
+git_hash = "81fb4f00c2cfe13814765968e09931ffa93b5138"
+git_describe = "v0.0-6140-g81fb4f00c"
+version_str = "0.0.post6140"
 version_tuple = (0, 0)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11908")
+    pversion = V("0.0.post6140")
 except ImportError:
     pass
