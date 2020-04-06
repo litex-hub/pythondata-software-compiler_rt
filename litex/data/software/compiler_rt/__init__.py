e64e3df728e3d8009596e80db8db29b96dc57b1d
LiteX <robot@timvideos.us>
2020-04-06 15:28:14 +0000

    Updating module to 0.0.post6183
    Updated data to v0.0-6140-g81fb4f00c based on 81fb4f00c2cfe13814765968e09931ffa93b5138 from https://git.llvm.org/git/compiler-rt.git/.
    > commit 81fb4f00c2cfe13814765968e09931ffa93b5138
    > Author: Chris Bieneman <beanz@apple.com>
    > Date:   Fri Nov 6 23:19:29 2015 +0000
    >
    >     [CMake] Need to filter ${arch}/*.c builtins as well as ${arch}/*.S builtins.
    >
    >     This was broken in r248542 when I refactored this to support builtins where ${arch} didn't match the directory prefix (i.e. armv7s).
    >
    >     git-svn-id: https://llvm.org/svn/llvm-project/compiler-rt/trunk@252365 91177308-0d34-0410-b5e6-96231b3b80d8
    >
    
    Updated using 0.0.post43 from https://github.com/litex-hub/litex-data-auto

--- litex/data/software/compiler_rt/__init__.py
+++ litex/data/software/compiler_rt/__init__.py
@@ -2,12 +2,44 @@ import os.path
 __dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
 data_location = os.path.join(__dir__, "data")
 src = "https://git.llvm.org/git/compiler-rt.git/"
-git_hash = "81fb4f00c2cfe13814765968e09931ffa93b5138"
-git_describe = "v0.0-6140-g81fb4f00c"
-version_str = "0.0.post6140"
-version_tuple = (0, 0)
+
+# Module version
+version_str = "0.0.post6183"
+version_tuple = (0, 0, 6183)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post6140")
+    pversion = V("0.0.post6183")
+except ImportError:
+    pass
+
+# Data version info
+data_version_str = "0.0.post6140"
+data_version_tuple = (0, 0, 6140)
+try:
+    from packaging.version import Version as V
+    pdata_version = V("0.0.post6140")
+except ImportError:
+    pass
+data_git_hash = "81fb4f00c2cfe13814765968e09931ffa93b5138"
+data_git_describe = "v0.0-6140-g81fb4f00c"
+data_git_msg = """\
+commit 81fb4f00c2cfe13814765968e09931ffa93b5138
+Author: Chris Bieneman <beanz@apple.com>
+Date:   Fri Nov 6 23:19:29 2015 +0000
+
+    [CMake] Need to filter ${arch}/*.c builtins as well as ${arch}/*.S builtins.
+    
+    This was broken in r248542 when I refactored this to support builtins where ${arch} didn't match the directory prefix (i.e. armv7s).
+    
+    git-svn-id: https://llvm.org/svn/llvm-project/compiler-rt/trunk@252365 91177308-0d34-0410-b5e6-96231b3b80d8
+
+"""
+
+# Tool version info
+tool_version_str = "0.0.post43"
+tool_version_tuple = (0, 0, 43)
+try:
+    from packaging.version import Version as V
+    ptool_version = V("0.0.post43")
 except ImportError:
     pass
--- test.py
+++ test.py
@@ -3,8 +3,18 @@ import os
 
 from litex.data.software import compiler_rt
 
-print("Found compiler_rt @ version", compiler_rt.version_str, "("+compiler_rt.git_hash+")")
+print("Found compiler_rt @ version", compiler_rt.version_str, "(with data", compiler_rt.data_version_str, ")")
+print()
 print("Data is in", compiler_rt.data_location)
 assert os.path.exists(compiler_rt.data_location)
-print("It contains:\n -", end=" ")
-print("\n - ".join(os.listdir(compiler_rt.data_location)))
+print("Data is version", compiler_rt.data_version_str, compiler_rt.data_git_hash)
+print("-"*75)
+print(compiler_rt.data_git_msg)
+print("-"*75)
+print()
+print("It contains:")
+for root, dirs, files in os.walk(compiler_rt.data_location):
+    dirs.sort()
+    for f in sorted(files):
+        path = os.path.relpath(os.path.join(root, f), compiler_rt.data_location)
+        print(" -", path)
