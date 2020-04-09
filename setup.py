c1ceb36566c16d102aa97798b26b6418011363cd
Tim 'mithro' Ansell <me@mith.ro>
2020-04-08 21:31:23 -0700

    Updating pythondata-software-compiler_rt to 0.0.post6188
    Updated data to v0.0-6140-g81fb4f00c based on 81fb4f00c2cfe13814765968e09931ffa93b5138 from https://git.llvm.org/git/compiler-rt.git/.
    > commit 81fb4f00c2cfe13814765968e09931ffa93b5138
    > Author: Chris Bieneman <beanz@apple.com>
    > Date:   Fri Nov 6 23:19:29 2015 +0000
    >
    >     [CMake] Need to filter ${arch}/*.c builtins as well as ${arch}/*.S builtins.
    >
    >     This was broken in r248542 when I refactored this to support builtins where ${arch} didn't match the directory prefix (i.e. armv7s).
    >
    >     git-svn-id: https://llvm.org/svn/llvm-project/compiler-rt/trunk@252365 91177308-0d34-0410-b5e6-96231b3b80d8
    >
    
    Updated using 0.0.post48 from https://github.com/litex-hub/litex-data-auto

--- setup.py
+++ setup.py
@@ -3,17 +3,18 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-from litex.data.software.compiler_rt import version_str
+from pythondata_software_compiler_rt import version_str
 
 setuptools.setup(
-    name="litex-data-software-compiler_rt",
+    name="pythondata-software-compiler_rt",
     version=version_str,
     author="LiteX Authors",
     author_email="litex@googlegroups.com",
-    description="Python module containing data files for using the LLVM Compiler RT Module software with LiteX.",
+    description="""\
+Python module containing data files for LLVM Compiler RT Module software.""",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/litex-hub/litex-data-software-compiler_rt",
+    url="https://github.com/litex-hub/pythondata-software-compiler_rt",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
@@ -22,6 +23,12 @@ setuptools.setup(
     python_requires='>=3.5',
     zip_safe=False,
     packages=setuptools.find_packages(),
-    package_data={'litex.data.software.compiler_rt': ['litex/data/software/compiler_rt/data/**']},
+    package_data={
+    	'software_compiler_rt': ['software_compiler_rt/data/**'],
+    },
     include_package_data=True,
+    project_urls={
+        "Bug Tracker": "https://github.com/litex-hub/pythondata-software-compiler_rt/issues",
+        "Source Code": "https://github.com/litex-hub/pythondata-software-compiler_rt",
+    },
 )
--- test.py
+++ test.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 import os
 
-from litex.data.software import compiler_rt
+import pythondata_software_compiler_rt
 
-print("Found compiler_rt @ version", compiler_rt.version_str, "(with data", compiler_rt.data_version_str, ")")
+print("Found compiler_rt @ version", pythondata_software_compiler_rt.version_str, "(with data", pythondata_software_compiler_rt.data_version_str, ")")
 print()
-print("Data is in", compiler_rt.data_location)
-assert os.path.exists(compiler_rt.data_location)
-print("Data is version", compiler_rt.data_version_str, compiler_rt.data_git_hash)
+print("Data is in", pythondata_software_compiler_rt.data_location)
+assert os.path.exists(pythondata_software_compiler_rt.data_location)
+print("Data is version", pythondata_software_compiler_rt.data_version_str, pythondata_software_compiler_rt.data_git_hash)
 print("-"*75)
-print(compiler_rt.data_git_msg)
+print(pythondata_software_compiler_rt.data_git_msg)
 print("-"*75)
 print()
 print("It contains:")
-for root, dirs, files in os.walk(compiler_rt.data_location):
+for root, dirs, files in os.walk(pythondata_software_compiler_rt.data_location):
     dirs.sort()
     for f in sorted(files):
-        path = os.path.relpath(os.path.join(root, f), compiler_rt.data_location)
+        path = os.path.relpath(os.path.join(root, f), pythondata_software_compiler_rt.data_location)
         print(" -", path)
